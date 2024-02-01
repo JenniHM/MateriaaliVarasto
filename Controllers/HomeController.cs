56824b9d6ce6580567fcaa79f5ba4cbc0bb3da1b
Jenni Mäenpää <jenni.maenpaa@student.careeria.fi>
2024-02-01 13:54:06 +0200

    Projektiin lisätty entiteettimalli Azuren SQL tietokannasta

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace MateriaaliVarasto.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Tietoja sovelluksesta.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Yhteystiedot.";
 
             return View();
         }
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MateriaaliVarasto.Models;
+
+namespace MateriaaliVarasto.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            MatskuniDBEntities1 db = new MatskuniDBEntities1();
+            List<Tuotteet> model = db.Tuotteet.ToList();
+            db.Dispose();
+            return View(model);
+        }
+    }
+}
