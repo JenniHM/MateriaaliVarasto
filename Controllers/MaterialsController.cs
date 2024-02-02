b8d62b367e1df3bdf52704147e04c5b3ce6f0e68
Jenni Mäenpää <jenni.maenpaa@student.careeria.fi>
2024-02-02 11:39:20 +0200

    Luotu CRUD-toiminnot tuotteille

---
+++ Controllers/MaterialsController.cs
@@ -0,0 +1,39 @@
+﻿using MateriaaliVarasto.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MateriaaliVarasto.Controllers
+{
+    public class MaterialsController : Controller
+    {
+        MatskuniDBEntities1 db = new MatskuniDBEntities1();
+        // GET: Materials
+        public ActionResult Index()
+        {
+            List<Materiaalit> model = db.Materiaalit.ToList();
+            return View(model);
+        }
+
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest); Shippers shippers = db.Shippers.Find(id);
+        //    if (shippers == null) return HttpNotFound(); returnView(shippers);
+        //}
+        //[HttpPost]
+        //[ValidateAntiForgeryToken] //Katsohttps://go.microsoft.com/fwlink/?LinkId=317598
+        //public ActionResult Edit([Bind(Include = "ShipperID,CompanyName,Phone")] Shippersshipper)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(shipper).State = EntityState.Modified; db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    returnView(shipper);
+        //}
+    }
+}
--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,21 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using MateriaaliVarasto.Models;
 
 namespace MateriaaliVarasto.Controllers
 {
     public class ProductController : Controller
     {
+        MatskuniDBEntities1 db = new MatskuniDBEntities1();
         // GET: Product
         public ActionResult Index()
         {
-            MatskuniDBEntities1 db = new MatskuniDBEntities1();
             List<Tuotteet> model = db.Tuotteet.ToList();
-            db.Dispose();
             return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Tuotteet tuotteet = db.Tuotteet.Find(id);
+            if (tuotteet == null) return HttpNotFound();
+            ViewBag.RyhmäID = new SelectList(db.Ryhmät, "RyhmäID", "Ryhmä", tuotteet.RyhmäID);
+            ViewBag.MateriaaliID = new SelectList(db.Materiaalit, "MateriaaliID", "Materiaali", tuotteet.MateriaaliID);
+            ViewBag.ValmistajaID = new SelectList(db.Valmistajat, "ValmistajaID", "Valmistaja", tuotteet.ValmistajaID);
+            return View(tuotteet);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public ActionResult Edit([Bind(Include = "TuoteID,Tuotenimi,ValmistajaID,RyhmäID,MateriaaliID,Pesty,Määrä")] Tuotteet tuote)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tuote).State = EntityState.Modified;
+                db.SaveChanges();
+                ViewBag.RyhmäID = new SelectList(db.Ryhmät, "RyhmäID", "Ryhmä", tuote.RyhmäID);
+                ViewBag.MateriaaliID = new SelectList(db.Materiaalit, "MateriaaliID", "Materiaali", tuote.MateriaaliID);
+                ViewBag.ValmistajaID = new SelectList(db.Valmistajat, "ValmistajaID", "Valmistaja", tuote.ValmistajaID);
+                return RedirectToAction("Index");
+            }
+            return View(tuote);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.RyhmäID = new SelectList(db.Ryhmät, "RyhmäID", "Ryhmä");
+            ViewBag.MateriaaliID = new SelectList(db.Materiaalit, "MateriaaliID", "Materiaali");
+            ViewBag.ValmistajaID = new SelectList(db.Valmistajat, "ValmistajaID", "Valmistaja");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TuoteID,Tuotenimi,ValmistajaID,RyhmäID,MateriaaliID,Pesty,Määrä")] Tuotteet tuote)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tuotteet.Add(tuote);
+                db.SaveChanges();
+                ViewBag.RyhmäID = new SelectList(db.Ryhmät, "RyhmäID", "Ryhmä", tuote.RyhmäID);
+                ViewBag.MateriaaliID = new SelectList(db.Materiaalit, "MateriaaliID", "Materiaali", tuote.MateriaaliID);
+                ViewBag.ValmistajaID = new SelectList(db.Valmistajat, "ValmistajaID", "Valmistaja", tuote.ValmistajaID);
+                return RedirectToAction("Index");
+            }
+            return View(tuote);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Tuotteet tuotteet = db.Tuotteet.Find(id);
+            if (tuotteet == null) return HttpNotFound();
+            return View(tuotteet);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tuotteet tuotteet = db.Tuotteet.Find(id); db.Tuotteet.Remove(tuotteet); 
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
