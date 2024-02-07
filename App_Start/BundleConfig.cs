4874269bac5780ebe008c63b64e00514509808ab
Jenni Mäenpää <jenni.maenpaa@student.careeria.fi>
2024-02-07 15:37:01 +0200

    Bootstrap kokeiluja

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -19,12 +19,17 @@ namespace MateriaaliVarasto
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap tryout.css",
+                      "~/Content/site.css"));
+
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
         }
     }
 }
